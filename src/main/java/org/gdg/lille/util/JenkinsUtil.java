d74358cceabfbd34f047dcc0894190aa114cfe20
Ludovic Borie <l.borie@free.fr>
2015-10-13 00:21:00 +0200

    New Http Client

--- src/main/java/org/gdg/lille/util/JenkinsUtil.java
+++ src/main/java/org/gdg/lille/util/JenkinsUtil.java
@@ -1,23 +1,10 @@
 package org.gdg.lille.util;
 
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.http.*;
 import com.google.api.services.compute.model.Instance;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.BasicAuthCache;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -31,8 +18,8 @@ import java.util.logging.Logger;
 public class JenkinsUtil {
 
     private static final Properties properties = new Properties();
-    private Logger logger = Logger.getLogger(JenkinsUtil.class.getSimpleName());
     private static JenkinsUtil INSTANCE;
+    private Logger logger = Logger.getLogger(JenkinsUtil.class.getSimpleName());
 
     public static JenkinsUtil getInstance() throws IOException {
         if (INSTANCE == null) {
@@ -43,18 +30,17 @@ public class JenkinsUtil {
     }
 
     public boolean isJobsRunningOrInstanceStarting(Instance instance) throws IOException {
-        String jenkinsUrl = "http://" + instance.getNetworkInterfaces().get(0).getAccessConfigs().get(0).getNatIP() + "/jenkins/api/json?depth=2";
+        String jenkinsUrl = "http://" + instance.getNetworkInterfaces().get(0).getAccessConfigs().get(0).getNatIP() + "/api/json?depth=2";
 
         // Jenkins available ?
-        final HttpGet httpGet = new HttpGet(jenkinsUrl);
-        HttpResponse response = execute(httpGet, instance);
-        if (response.getStatusLine().getStatusCode() != 200) {
+        HttpResponse response = execute("GET", new GenericUrl(jenkinsUrl), null);
+        if (response.getStatusCode() != 200) {
             return true;
         }
 
         // Is any job running ?
         Gson gson = new GsonBuilder().create();
-        Map content = gson.fromJson(new InputStreamReader(response.getEntity().getContent(), "UTF-8"), Map.class);
+        Map content = gson.fromJson(new InputStreamReader(response.getContent(), "UTF-8"), Map.class);
         List<Map> jobs = (List<Map>) content.get("jobs");
         for (Map job : jobs) {
             boolean jobInQueue = (boolean) job.get("inQueue");
@@ -76,22 +62,21 @@ public class JenkinsUtil {
         // Waiting 10 seconds to permit the launch of Jenkins
         Thread.sleep(10000);
 
-        String jenkinsUrl = "http://" + instance.getNetworkInterfaces().get(0).getAccessConfigs().get(0).getNatIP() + "/jenkins";
+        String jenkinsUrl = "http://" + instance.getNetworkInterfaces().get(0).getAccessConfigs().get(0).getNatIP();
         logger.log(Level.INFO, "Jenkins URL : " + jenkinsUrl);
 
-        final HttpGet httpGet = new HttpGet(jenkinsUrl + "/api/json?depth=1");
         try {
-            HttpResponse response = execute(httpGet, instance);
-            while (response.getStatusLine().getStatusCode() != 200) {
+            HttpResponse response = execute("GET", new GenericUrl(jenkinsUrl + "/api/json?depth=1"), null);
+            while (response.getStatusCode() != 200) {
                 Thread.sleep(1000);
-                logger.log(Level.INFO, "Impossible to access to Jenkins, http response : " + response.getStatusLine().getStatusCode() + ". Retrying.");
-                response = execute(httpGet, instance);
+                logger.log(Level.INFO, "Impossible to access to Jenkins, http response : " + response.getStatusCode() + ". Retrying.");
+                response = execute("GET", new GenericUrl(jenkinsUrl + "/api/json?depth=1"), null);
             }
 
 
             // Is the job exists ?
             Gson gson = new GsonBuilder().create();
-            Map content = gson.fromJson(new InputStreamReader(response.getEntity().getContent(), "UTF-8"), Map.class);
+            Map content = gson.fromJson(new InputStreamReader(response.getContent(), "UTF-8"), Map.class);
             List<Map> jobs = (List<Map>) content.get("jobs");
             boolean jobExists = false;
             for (Map job : jobs) {
@@ -102,11 +87,10 @@ public class JenkinsUtil {
             }
 
             if (jobExists) {
-                final HttpPost build = new HttpPost(jenkinsUrl + "/job/" + jobName + "/build");
                 logger.log(Level.INFO, "Job URL : " + jenkinsUrl + "/job/" + jobName);
-                HttpResponse buildResponse = execute(build, instance);
-                if (buildResponse.getStatusLine().getStatusCode() != 201) {
-                    logger.log(Level.SEVERE, "Impossible to launch the job, http response : " + buildResponse.getStatusLine().getStatusCode());
+                HttpResponse buildResponse = execute("POST", new GenericUrl(jenkinsUrl + "/job/" + jobName + "/build"), null);
+                if (buildResponse.getStatusCode() != 201) {
+                    logger.log(Level.SEVERE, "Impossible to launch the job, http response : " + buildResponse.getStatusCode());
                 } else {
                     logger.log(Level.INFO, "job launched!");
                 }
@@ -119,26 +103,22 @@ public class JenkinsUtil {
         }
     }
 
-    private HttpResponse execute(HttpRequest httpRequest, Instance instance) throws IOException {
+    private HttpResponse execute(String method, GenericUrl url, HttpContent content) throws IOException {
 
-        CredentialsProvider provider = new BasicCredentialsProvider();
-        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(properties.getProperty("jenkins.user"), properties.getProperty("jenkins.token"));
-        provider.setCredentials(AuthScope.ANY, credentials);
-        HttpClient httpClient = HttpClientBuilder
-                .create()
-                .setDefaultCredentialsProvider(provider)
-                .build();
+        logger.log(Level.INFO, "Executing this method : " + method);
+        logger.log(Level.INFO, "to This url : " + url);
 
-        // Create AuthCache instance
-        AuthCache authCache = new BasicAuthCache();
-        // Generate BASIC scheme object and add it to the local auth cache
-        BasicScheme basicAuth = new BasicScheme();
-        HttpHost targetHost = new HttpHost(instance.getNetworkInterfaces().get(0).getAccessConfigs().get(0).getNatIP(), 80, "http");
-        authCache.put(targetHost, basicAuth);
-        HttpClientContext context = HttpClientContext.create();
-        context.setCredentialsProvider(provider);
-        context.setAuthCache(authCache);
-
-        return httpClient.execute(targetHost, httpRequest, context);
+        HttpTransport httpTransport = UrlFetchTransport.getDefaultInstance();
+        HttpRequestFactory requestFactory =
+                httpTransport.createRequestFactory(new HttpRequestInitializer() {
+                    @Override
+                    public void initialize(com.google.api.client.http.HttpRequest httpRequest) throws IOException {
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.setBasicAuthentication(properties.getProperty("jenkins.user"), properties.getProperty("jenkins.token"));
+                        httpRequest.setHeaders(headers);
+                        httpRequest.setThrowExceptionOnExecuteError(false);
+                    }
+                });
+        return requestFactory.buildRequest(method, url, content).setThrowExceptionOnExecuteError(false).execute();
     }
 }
